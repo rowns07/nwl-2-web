0a8c0134b98254f958c68bb0b84d2468e022312a
Diego Souza <diegosouza@tradecom.com.br>
2020-08-04 16:48:51 -0300

    Adicionando Page TeacherList e component TeacherItem

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import './styles.css';
+
+
+const TeacherItem = () => {
+    return(
+        <article className="teacher-item">
+            <header>
+                <div>
+                    <img src="https://avatars3.githubusercontent.com/u/6974831?s=460&u=1a4fe9ab2da3948723361308911fd55a36b757d2&v=4" alt="Diego Souza" />
+                    <strong>
+                        Diego Souza
+                    </strong>
+                    <span>
+                        ED FISICA
+                    </span>
+                </div>
+            </header>
+
+            <p>
+                Mussum Ipsum, cacilds vidis litro abertis. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose
+                    <br /><br />
+                    HAHAHAHAHAH AHHKDJS KDJSKDJKSJDSKD
+                    </p>
+            <footer>
+                <p>
+                    Preço/hora
+                        <strong>
+                        R$ 80,00
+                        </strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                        Entrar em contato
+                        </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -5,6 +5,7 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import {Link} from 'react-router-dom';
 
 import './styles.css';
 
@@ -24,14 +25,14 @@ function Landing(){
                 className="hero-image"
             />
             <div className="buttons-container">
-                <a href="" className="study">
+                <Link to="/study" className="study">
                   <img src={studyIcon} alt="Estudar"/>
                 Estudar
-                </a>
-                <a href="" className="give-classes">
+                </Link>
+                <Link to="/give-classes" className="give-classes">
                   <img src={giveClassesIcon} alt="Dar Aulas"/>
                 Dar Aulas
-                </a>
+                </Link>
             </div>
                 <span className="total-connections">
                     Total de 200 conexões já realizadas
